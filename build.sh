7e5ae93be2d5f5e73ac4537a60fd099d7205a278
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-03-23 17:53:53 +0900

    add tophat 20220323

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-for tag in latest #20190523
+for tag in 20220323 #latest #20190523
 do
-    docker build -t rnakato/anaconda:$tag .
+    docker build -f Dockerfile.$tag -t rnakato/anaconda:$tag .
     docker push rnakato/anaconda:$tag
+    docker build -f Dockerfile.$tag -t rnakato/anaconda .
+    docker push rnakato/anaconda
 done
